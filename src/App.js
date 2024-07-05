5a3b88806b58c89c9401dd83310a1aa20ccb1fa8
KHELIL Akram <khelili.akram@courrier.uqam.ca>
2024-07-05 17:10:40 -0400

    responsive version of the login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import LoginPage from './LoginPage';
 import Card from './MyCard';
 import Navbar from './Navbar';
@@ -7,55 +7,56 @@ import FormDialog from './FormDialog';
 import Home from './Home';
 import SignupDialog from './SignupDialog';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-
-
-
-
+import './style/App.css';
+import loginImage from './login.jpg';
 
 function App() {
   const [open, setOpen] = useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    }
-
-    const handleClickClose = () => {
-        setOpen(false);
-    }
   const [openSignup, setopenSignup] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClickClose = () => {
+    setOpen(false);
+  };
+
   const handleClickOpenSignup = () => {
     setopenSignup(true);
-  }
+  };
+
   const handleCloseSignup = () => {
     setopenSignup(false);
-  }
+  };
+
   return (
     <Router>
       <Routes>
         <Route
           path='/'
-          element = {
+          element={
             <>
-            <LoginPage handleClickOpenSignup = {handleClickOpenSignup}/>
-            <SignupDialog openSignup={openSignup} handleCloseSignup={handleCloseSignup} />
+              <header className='header'>
+                <div className='login-container'>
+                  <LoginPage handleClickOpenSignup={handleClickOpenSignup} />
+                </div>
+              </header>
+              <SignupDialog openSignup={openSignup} handleCloseSignup={handleCloseSignup} />
             </>
           }
         />
         <Route
           path='/dashboard'
-          element = {
+          element={
             <>
-              <Navbar handleClickOpen={handleClickOpen}/>
-              <Home/>
-              <FormDialog open={open} handleClose={handleClickClose}/>
+              <Navbar handleClickOpen={handleClickOpen} />
+              <Home />
+              <FormDialog open={open} handleClose={handleClickClose} />
             </>
           }
         />
       </Routes>
-      
-      
-      
     </Router>
   );
 }
