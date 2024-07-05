c37d018af7dd0815f17f5d1f0cf58e65a29d8b04
Khalil Snoussi <136627481+khalilsnoussi@users.noreply.github.com>
2024-07-05 11:33:34 -0400

    Update App.js to take into account different routes, "/" and "/dashboard"

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import svgImg from './image.svg';
 import FormDialog from './FormDialog';
 import Home from './Home';
 import SignupDialog from './SignupDialog';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 
 
 
 
 
 function App() {
-  /*
   const [open, setOpen] = useState(false);
 
     const handleClickOpen = () => {
@@ -21,7 +22,7 @@ function App() {
 
     const handleClickClose = () => {
         setOpen(false);
-    }*/
+    }
   const [openSignup, setopenSignup] = useState(false);
   const handleClickOpenSignup = () => {
     setopenSignup(true);
@@ -30,14 +31,32 @@ function App() {
     setopenSignup(false);
   }
   return (
-    <>
-      {/*<Navbar handleClickOpen={handleClickOpen}/>
-      <Home/>
-      <FormDialog open={open} handleClose={handleClickClose}/>*/}
-      <LoginPage handleClickOpenSignup = {handleClickOpenSignup}/>
-      <SignupDialog openSignup={openSignup} handleCloseSignup={handleCloseSignup} />
+    <Router>
+      <Routes>
+        <Route
+          path='/'
+          element = {
+            <>
+            <LoginPage handleClickOpenSignup = {handleClickOpenSignup}/>
+            <SignupDialog openSignup={openSignup} handleCloseSignup={handleCloseSignup} />
+            </>
+          }
+        />
+        <Route
+          path='/dashboard'
+          element = {
+            <>
+              <Navbar handleClickOpen={handleClickOpen}/>
+              <Home/>
+              <FormDialog open={open} handleClose={handleClickClose}/>
+            </>
+          }
+        />
+      </Routes>
       
-    </>
+      
+      
+    </Router>
   );
 }
 
